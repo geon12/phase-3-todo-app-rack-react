120211176aa0d788a10977f0414d075a4da8e524
Cameron <camcreel@gmail.com>
2021-08-02 20:35:04 -0400

    Completed assignment

--- back-end/app/application.rb
+++ back-end/app/application.rb
@@ -2,17 +2,83 @@ require 'pry'
 require 'json'
 
 class Application
-
+  #shotgun -p 3000
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    content_type = { 'Content-Type' => 'application/json' }
+    if req.path == '/tasks' && req.get?
+      tasks = Task.all.map {|task| {id: task.id, text: task.text, category: task.category.name} } 
+      return [200,content_type, [ tasks.to_json ]]
+    
+    elsif req.path == '/tasks' && req.post?
+      
+      body = JSON.parse(req.body.read)
+      task = Task.create(text:body["text"], category: Category.find_by(name:body["category"]))
+      
+      task = {id: task.id, text: task.text, category: task.category.name}
+      return [
+        200,
+        content_type,
+        [ task.to_json ]
+      ]
+    elsif req.path.match(/tasks/) && req.get?
+      id = req.path.split('/')[2]
+      task = Task.find_by_id(id)
+      
+      if task 
+        task = {id:task.id, text: task.text, category: task.category.name}
+        return [
+          200,
+          content_type,
+          [ task.to_json ]
+        ]
+      else
+        return [
+          204,
+          content_type,
+          [ {}.to_json ]
+        ]
+      end
+    
+    elsif req.path.match(/tasks/) && req.patch?
+      id = req.path.split('/')[2]
+      task = Task.find_by_id(id)
+      body = JSON.parse(req.body.read)
+      if task
+        task.update(body)
+        task = {id:task.id,text: task.text, category: task.category}
+        return [
+          200,
+          content_type,
+          [task.to_json ]
+        ]
+      else
+        return [
+          204,
+          content_type,
+          [ {}.to_json ]
+        ]
+      end
+    
+    elsif req.path.match(/tasks/) && req.delete?
+      id = req.path.split('/')[2]
+      task = Task.find_by_id(id)
 
-    if req.path.match(/test/) 
+      task.destroy
 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "response success"}.to_json ]]
+      return [
+        204,
+        {},
+        ['']
+      ]
 
     else
-      resp.write "Path Not Found"
+      return [
+        404,
+        {},
+        ['Page not found']
+      ]
 
     end
 
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -4,9 +4,18 @@ Task.destroy_all
 
 puts "Seeding Categories..."
 
+c1 = Category.create(name: "Code")
+c2 = Category.create(name: "Food")
+c3 = Category.create(name: "Money")
+c4 = Category.create(name: "Misc")
+
 # create categories
 
 puts "Seeding tasks..."
+
+20.times do
+    Task.create(text:Faker::Lorem.paragraph,category:[c1,c2,c3,c4].sample)
+end
 
 # create tasks
 
